5ce68ce4b9c82dd18105e45f16dcd58a7476e1ab
Yu Long Zhang <zgyulg@cn.ibm.com>
2024-09-01 20:26:47 +0800

    update leedcode

---
+++ go/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var a int = 10
+var c int
+
+func main() {
+	b := 0
+	c = 10
+	fmt.Println("hello world")
+	fmt.Println("a=", a)
+	fmt.Println("b=", b)
+	fmt.Println("c=", c)
+}
